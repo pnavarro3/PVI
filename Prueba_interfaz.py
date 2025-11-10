60083df6a6efebc05146d997835dda933092379e
pnavarro3 <alu0101844633@ull.edu.es>
2025-11-10 17:45:28 +0000

    Add files via upload

--- Prueba_interfaz.py
+++ Prueba_interfaz.py
@@ -12,12 +12,15 @@ except serial.SerialException as e:
     print(f"Error al abrir el puerto serial: {e}")
     exit()
 
-def enviar_comando(comando):
+def enviar_comando(comando, etiqueta=None):
     ser.write((comando + "\n").encode('utf-8'))
     time.sleep(3)  # Espera a que Arduino escriba en el buffer
 
     respuesta = ser.readline().decode('utf-8').strip()
-    if not respuesta:
+    if respuesta:
+        if etiqueta:
+            print(f"{etiqueta}: {respuesta}")
+    else:
         print("No se recibió respuesta del dispositivo.")
 
 def menu():
@@ -26,7 +29,7 @@ def menu():
     print("2. Llenar")
     print("3. Vaciar")
     print("4. Parar")
-    print("5. Tare / Calibrar")
+    print("5. Tara / Calibrar")
     print("6. Medir peso/volumen")
     print("7. Enviar consigna")
     print("8. Salir")
@@ -36,7 +39,7 @@ while True:
     opcion = menu()
 
     if opcion == "1":
-        enviar_comando("SYSTem:VERSion?")
+        enviar_comando("SYSTem:VERSion?", etiqueta="Dispositivo")
     
     elif opcion == "2":
         enviar_comando("STATus:OPERation:LLEnar")
@@ -51,7 +54,7 @@ while True:
         enviar_comando("STATus:OPERation:TARCAL")
     
     elif opcion == "6":
-        enviar_comando("STATus:VOLumen?")
+        enviar_comando("STATus:VOLumen?", etiqueta="Peso")
     
     elif opcion == "7":
         valor = input("Introduce valor de consigna: ")
@@ -64,4 +67,3 @@ while True:
     
     else:
         print("Opción no válida. Intenta otra vez.")
-
